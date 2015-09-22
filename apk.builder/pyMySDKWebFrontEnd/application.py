f21a72716ccd591508411db7373ddd0758f806eb
leenjewel <leenjewel@gmail.com>
2015-09-22 14:01:52 +0800

    Web Server 增加 Daemon 启动方式

--- apk.builder/pyMySDKWebFrontEnd/application.py
+++ apk.builder/pyMySDKWebFrontEnd/application.py
@@ -14,7 +14,8 @@
 # limitations under the License.
 #
 
-import sys,os
+import sys,os,time,atexit
+from signal import SIGTERM
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
@@ -90,6 +91,118 @@ class Application(tornado.web.Application) :
 
 
 
+class ApplicationDaemon(object) :
+
+    def __init__(self, user_settings) :
+        self.settings = user_settings
+
+
+    def pidfile(self) :
+        pid_file = self.settings.get("pidfile")
+        if None is pid_file or len(pid_file) == 0 :
+            pid_file = os.path.join(os.getcwd(), "mysdk-server.pid")
+        return pid_file
+
+
+    def daemonize(self) :
+        try :
+            if os.fork() > 0 :
+                sys.exit(0)
+        except OSError, e :
+            sys.stderr.write("fork #1 failed : %d %s\n"  %(e.errno, e.strerror))
+            sys.exit(1)
+
+        os.setsid()
+        os.umask(0)
+
+        try :
+            if os.fork() > 0 :
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed : %d %s\n"  %(e.errno, e.strerror))
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        stdout_file = self.settings.get("stdout")
+        if None is stdout_file or len(stdout_file) == 0 :
+            stdout_file = os.path.join(os.getcwd(), "std.out")
+        stdout = file(stdout_file, "a+")
+        os.dup2(stdout.fileno(), sys.stdout.fileno())
+
+        stderr_file = self.settings.get("stderr")
+        if None is stderr_file or len(stderr_file) == 0 :
+            stderr_file = os.path.join(os.getcwd(), "std.err")
+        stderr = file(stderr_file, "a+", 0)
+        os.dup2(stderr.fileno(), sys.stderr.fileno())
+
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        pid_file = self.pidfile()
+        pid_fp = open(pid_file, 'w+')
+        pid_fp.write("%s\n"  %(pid))
+        pid_fp.close()
+
+
+    def delpid(self) :
+        os.remove(self.pidfile())
+
+
+    def getpid(self) :
+        try :
+            pid_fp = file(self.pidfile(), "r")
+            pid = int(pid_fp.read().strip())
+            pid_fp.close()
+        except IOError :
+            pid = None
+        return pid
+
+
+    def start(self) :
+        pid = self.getpid()
+        if pid :
+            sys.stderr.write("pid file %s already exist. Daemon already running?\n"  %(self.pidfile()))
+            sys.exit(1)
+
+        self.daemonize()
+
+        Application(self.settings).run(self.settings.get("port", 8080))
+
+
+    def stop(self) :
+        pid = self.getpid()
+        if not pid :
+            sys.stderr.write("pid file %s not exists. Daemon not running?\n"  %(self.pidfile()))
+            return
+
+        try :
+            while True :
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, e :
+            error = str(e)
+            if error.find("No such process") > 0 :
+                if os.path.exists(self.pidfile()) :
+                    os.remove(self.pidfile())
+            else :
+                sys.stderr.write(error)
+                sys.exit(1)
+
+
+    def restart(self) :
+        self.stop()
+        time.sleep(0.5)
+        self.start()
+
+
+    def run(self, port = None) :
+        if port :
+            self.settings["port"] = port
+        self.start()
+
+
+
 if __name__ == "__main__" :
     import json, argparse
     parser = argparse.ArgumentParser()
@@ -97,6 +210,8 @@ if __name__ == "__main__" :
     parser.add_argument('config', help = "Config file path")
 
     parser.add_argument('--port', default=8080, help = "Listen port")
+
+    parser.add_argument('--daemon', action = 'store_true', default = False, help = "Start server with daemon")
 
     args = parser.parse_args()
 
@@ -107,5 +222,8 @@ if __name__ == "__main__" :
     fp.close()
     os.chdir(os.path.split(args.config)[0])
 
-    Application(settings).run(args.port)
+    if args.daemon :
+        ApplicationDaemon(settings).run(args.port)
+    else :
+        Application(settings).run(args.port)
 
